3e4099d05fdcae16eeba2bbd7ee7e80c9ff2a119
Chukwugozie Okeke <cmoedes27@gmail.com>
2024-07-05 07:52:11 -0600

    Added Player.py

---
+++ python_scripts/Player.py
@@ -0,0 +1,15 @@
+import constants
+
+class Player:
+    def __init__(self):
+        self.workers = {}
+        pass
+    def get_valid_moves(self):
+        pass
+    def execute_move(self):
+        pass
+    def execute_turn(self):
+        pass
+    def __str__(self):
+        pass
+    pass
