6767cb9edad05052ae0a9a03c9ebbbefb008af5f
Chukwugozie Okeke <cmoedes27@gmail.com>
2024-07-05 12:34:21 -0600

    Removed Player get_valid_moves method

--- python_scripts/Player.py
+++ python_scripts/Player.py
@@ -10,17 +10,9 @@ class Player:
     def set_worker_position(self, worker, x, y):
         self.workers[worker]["x"] = x
         self.workers[worker]["y"] = y
-    
-    def get_valid_moves(self, Board):
-        valid_moves = {}
-        board_schema = Board.get_schema()
-        for worker in self.workers:
-            pass
-        return valid_moves
 
     def __str__(self):
-        retstr = ""
-        retstr+="Player:"
+        retstr = "Player: "
         for i, worker in enumerate(self.workers):
             retstr+= f"\tWorker {i} || {worker}\n"
         return retstr
