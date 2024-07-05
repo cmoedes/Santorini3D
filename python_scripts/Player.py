e0420a9f58aac45ecb62ba74729c99bd331ff5fa
Chukwugozie Okeke <cmoedes27@gmail.com>
2024-07-05 10:46:14 -0600

    Updated Player.py

--- python_scripts/Player.py
+++ python_scripts/Player.py
@@ -1,15 +1,37 @@
-import constants
+from constants import PLAYER_WORKERS
 
 class Player:
     def __init__(self):
-        self.workers = {}
-        pass
-    def get_valid_moves(self):
-        pass
-    def execute_move(self):
-        pass
-    def execute_turn(self):
+        self.workers = []
+        self.captured = False
+        for i in range(PLAYER_WORKERS):
+            self.workers.append({
+                "x": -1, 
+                "y":-1,
+                "cap":False
+            })
+    
+    def set_worker_positions(self, workers, positions):
+        if(type(workers) is list):
+            for i, (worker, position) in enumerate(zip(workers, positions)):
+                self.workers[worker]["x"] = position["x"]
+                self.workers[worker]["y"] = position["y"]
+            return
+        self.workers[workers]["x"] = positions["x"]
+        self.workers[workers]["y"] = positions["y"]
+    
+    def get_valid_moves(self, Board):
+        valid_moves = {}
+        board_schema = Board.get_schema()
+        for worker in self.workers:
+            pass
+        return valid_moves
+    
+    def execute_move(self, Board):
         pass
     def __str__(self):
-        pass
-    pass
+        print("Player:")
+        for i, worker in enumerate(self.workers):
+            print(f"\tWorker ${i} - (x, y): (${worker["x"]}, ${worker["y"]})")
+
+
