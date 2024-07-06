8b7414e546781c33d979f97a8a8686755f143866
Chukwugozie Okeke <cmoedes27@gmail.com>
2024-07-06 11:51:08 -0600

    Added init and worker methods to Player.py

--- python_scripts/Player.py
+++ python_scripts/Player.py
@@ -1,20 +1,39 @@
-from constants import PLAYER_WORKERS
+from constants import PLAYER_WORKERS, BOARD_SIZE, BASE_WORKER_OBJ
 
 class Player:
     def __init__(self):
         self.workers = []
         self.captured = False
-        for i in range(PLAYER_WORKERS):
-            self.workers.append({"x": -1, "y":-1, "cap":False})
+    
+    @staticmethod
+    def get_empty_cell(Board):
+        print("Player Worker Initialization:")
+        x_pos = int(input(f"\tEnter Worker x: "))
+        y_pos = int(input(f"\tEnter Worker y: "))
+        
+        on_board = (x_pos>=0 or x_pos<BOARD_SIZE) or (y_pos>=0 or y_pos<BOARD_SIZE)
+        occupied = Board.schema[y_pos][x_pos]<0
+        while(not on_board or occupied):
+            print("Invalid Selection. Please try again.")
+            x_pos = int(input(f"\tEnter Worker x: "))
+            y_pos = int(input(f"\tEnter Worker y: "))
+        return x_pos, y_pos
     
     def set_worker_position(self, worker, x, y):
-        self.workers[worker]["x"] = x
-        self.workers[worker]["y"] = y
+        self.workers[worker] = {**self.workers[worker], "x": x, "y": y}
+
+    def init_workers(self, Board):
+        for worker in range(PLAYER_WORKERS):
+            x_pos, y_pos = Player.get_empty_cell(Board)
+            self.workers.append(BASE_WORKER_OBJ.copy())
+            self.set_worker_position(worker, x_pos, y_pos)
+            Board.history.append({})
+            Board.update_schema()
 
     def __str__(self):
-        retstr = "Player: "
+        st = "Player: \n"
         for i, worker in enumerate(self.workers):
-            retstr+= f"\tWorker {i} || {worker}\n"
-        return retstr
+            st+= f"\t|Worker{i}|: {worker}\n"
+        return st
 
 
