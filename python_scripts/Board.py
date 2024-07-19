02c5a928d8feb551795ae4fe4f6c009b0644a878
Chukwugozie Okeke <cmoedes27@gmail.com>
2024-07-18 23:10:47 -0600

    Updated Board.py

--- python_scripts/Board.py
+++ python_scripts/Board.py
@@ -1,4 +1,4 @@
-from constants import BOARD_SIZE, TOTAL_PLAYERS, PLAYER_WORKERS
+from constants import BOARD_SIZE, TOTAL_PLAYERS, PLAYER_WORKERS, BASE_CELL_OBJ
 from Player import Player
 
 class Board:
@@ -7,11 +7,11 @@ class Board:
         self.history = []
         self.schema = []
         self.Players = []
-        self.game_Over = False
+        self.game_over = False
 
     def init_schema(self):
         for row in range(BOARD_SIZE):
-            self.schema.append([0]*BOARD_SIZE)
+            self.schema.append([BASE_CELL_OBJ.copy() for _ in range(BOARD_SIZE)])
 
     def init_players(self):
         for i in range(TOTAL_PLAYERS):
@@ -21,15 +21,52 @@ class Board:
     def simulate_game(self):
         self.init_schema()
         self.init_players()
+        player_turn = 0
+        while(not self.game_over):
+            player_turn = player_turn % TOTAL_PLAYERS
+            self.Players[player_turn].execute_turn(self)
+            player_turn+=1
+        print("Player won. Game Over.")
 
     def update_schema(self):
-        pass
+        for i in range(self.history_pointer, len(self.history)):
+            event = self.history[i]
+            
+            if(event['type'] == 'set'):
+                x, y = event['x'], event['y']
+                self.schema[y][x]['Player'] = event['Player']
+                self.schema[y][x]['Worker'] = event['Worker']
+                event['Player'].set_worker_position(event['Worker'], event['x'], event['y'])
+            
+            elif(event['type'] == 'move'):
+                Worker = event['Player'].workers[event['Worker']]
+                xw, yw, x, y = Worker['x'], Worker['y'], event['x'], event['y']
+                self.schema[yw][xw]['Player'] = None
+                self.schema[yw][xw]['Worker'] = None
+                self.schema[y][x]['Player'] = event['Player']
+                self.schema[y][x]['Worker'] = event['Worker']
+                event['Player'].set_worker_position(event['Worker'], event['x'], event['y'])
+            
+            elif(event['type'] == 'build'):
+                x, y = event['x'], event['y']
+                self.schema[y][x]['Height'] = self.schema[y][x]['Height'] + 1
+                
+        self.history_pointer = len(self.history)
 
-    def Reset(self):
+    def reset(self):
+        self.history_pointer = 0
         self.history.clear()
         self.schema.clear()
         self.Players.clear()
         self.game_over = False
 
     def __str__(self):
-        return self.schema
+        st="\n"
+        for row in self.schema:
+            for cell in row:
+                item = str(cell['Height']) or "0"
+                if(cell['Player'] is not None):
+                    item = "X"
+                st+=item
+            st+="\n"
+        return st
