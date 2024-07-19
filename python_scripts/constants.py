ee3bb436abc25ee28b6ca82b4d16a43ddb0923c8
Chukwugozie Okeke <cmoedes27@gmail.com>
2024-07-18 23:09:39 -0600

    Added constants to constants.py

--- python_scripts/constants.py
+++ python_scripts/constants.py
@@ -1,4 +1,6 @@
 BOARD_SIZE = 5
 TOTAL_PLAYERS = 2
-PLAYER_WORKERS = 2
-BASE_WORKER_OBJ = {"x": None, "y": None, "cap": False}
+PLAYER_WORKERS = 1
+BASE_WORKER_OBJ = {"x": None, "y": None}
+BASE_CELL_OBJ = {"Height": 0, "Player": None, "Worker": None}
+DIRS = [[1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1]]
